b9128394d6768b37418ccfdbf9907b5e439ba8c0
geoivannikov <55244414+geoivannikov@users.noreply.github.com>
2025-10-16 14:16:42 +0200

    Update Contents.swift

--- HabitClassifierPlayground.playground/Contents.swift
+++ HabitClassifierPlayground.playground/Contents.swift
@@ -6,5 +6,5 @@ let data = try MLDataTable(contentsOf: fileURL)
 
 let model = try MLTextClassifier(trainingData: data, textColumn: "habit", labelColumn: "category")
 
-let saveURL = URL(fileURLWithPath: "/Users/s0143190/Desktop/Pet/HabitTracker/HabitTracker/ml/HabitClassifier.mlmodel")
+let saveURL = URL(fileURLWithPath: "/HabitTracker/HabitTracker/ml/HabitClassifier.mlmodel")
 try model.write(to: saveURL)
